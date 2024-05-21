5198011f32b993e0b29c7e6682454392a1252928
Abraham Galindo <abraham.garo@gmail.com>
2024-05-20 20:40:58 -0600

    feat: add coverage to Codecov

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       include: ['src/components/**/*.{ts,tsx}', 'src/helpers/**/*.{ts,tsx}'],
       // if you want to exclude some files
       exclude: [],
-      reporter: ['html', 'text-summary'],
+      reporter: ['html', 'text-summary', 'cobertura'],
       thresholds: {
         functions: 50,
         lines: 50,
